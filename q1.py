25c29cd4931be64a0e1c62f9bbaa6028c49d94db
jerrytohvan <jerrytohvan@gmail.com>
2019-03-25 14:16:44 +0800

    First greedy algorithm implemented successfully

--- q1.py
+++ q1.py
@@ -1,7 +1,8 @@
 # Team ID: G4T12
-
 import  itertools
- 
+import numpy as np
+
+
 def store_in_dict(locations):
     dd = {}
     for loc in locations:
@@ -11,9 +12,34 @@ def store_in_dict(locations):
         else:
             start = lst[0]
             dest = lst[1]
-            val = lst[2]
+            val = int(lst[2])
             dd[start][dest] = val
     return dd
+
+
+def firstGreedyShortestPath(dict_loc, start_location, all_cities, k):
+    #all_cities: ['RUS', 'CAN', 'SIN', 'KOR', 'CHN', 'MEX', 'AUS', 'GMY', 'FRN', 'SPN']
+    #k: length of orders 
+    selected_order_ids = []
+
+    curr = start_location
+    while len(selected_order_ids) != k:
+        #least dist 
+        mini = 99999999
+        low_k = ''
+        index=0
+        for j in  range (0, len(all_cities)):
+            city = all_cities[j]
+            if j not in selected_order_ids and curr != all_cities[j] and mini > dict_loc[curr][city]:
+                mini = dict_loc[curr][city]
+                low_k = city
+                index = j
+        selected_order_ids.append(index)
+        curr=low_k
+    return tuple(selected_order_ids)
+
+def returnOrders(list_orders, orders):
+    return [orders[i] for i in list_orders]
 
 
 def schedule1(locations, start_location, number_of_trucks, orders):
@@ -26,110 +52,35 @@ def schedule1(locations, start_location, number_of_trucks, orders):
     # [(2, 150, 'CAN'), (6, 250, 'MEX'), (10, 230, 'SPN')],
     # [(3, 250, 'SIN'), (7, 350, 'AUS')],
     # [(4, 300, 'KOR'), (8, 270, 'GMY')]]
-    orders_left = orders
-
     max_list = []
     for i in range(number_of_trucks):
         max_list.append([])
 
-    # print("Start :" + start_location)
-
-
-    #approx equal dist per no trucks:
-    k = len(orders) // number_of_trucks
-    dict_locations = store_in_dict(locations)
-
-    all_orders_index = ([i for i in range(0,len(orders))])
-    all_cities =  ([i[2] for i in orders])
+    dict_loc= store_in_dict(locations)
+  
     
-    #all combinations on equal weight
-    permuts_orders_index = list(itertools.combinations(all_orders_index,k))
+    no_ordes = len(orders)
+    copy_orders = orders
 
-    #calculate all possible travel cost:
-    all_travel_cost = []
-    for possible_order in permuts_orders_index:
-        start_cost = int(dict_locations[start_location][orders[possible_order[0]][2]])
-        end_cost = int(dict_locations[orders[possible_order[-1]][2]][start_location])
-        middle_cost = 0
-        for i in range(0, len(possible_order)-1):
-            middle_cost+= int(dict_locations[orders[possible_order[i]][2]][orders[possible_order[i+1]][2]])
-        all_travel_cost.append(start_cost+middle_cost+end_cost)
-   
-    #add index before sorting to the smallest correspoding to `all_travel_cost` & `permuts_orders_index`
+    no_per_trucks = len(orders) // number_of_trucks +1
+    max_list_index = 0
+    while number_of_trucks > 0: 
+        #base case
+        all_cities =  ([i[2] for i in orders])
+        if number_of_trucks == 1:
+            shortest_path = firstGreedyShortestPath(dict_loc,start_location, all_cities,len(orders))
+            max_list[max_list_index] = returnOrders(shortest_path, orders)
+            number_of_trucks = 0
+        else:
+            shortest_path = firstGreedyShortestPath(dict_loc,start_location, all_cities,len(orders))
+            max_list[max_list_index] = returnOrders(shortest_path[1:no_per_trucks], orders)
 
-    all_travel_cost_index = []
-    for i in range(0, len(all_travel_cost)):
-        all_travel_cost_index.append([i,all_travel_cost[i]])
-    all_travel_cost_index.sort(key=lambda x: x[1])
-    #getting corresponding index
-    # print(all_travel_cost_index)
+            #remove orders 
+            for i in shortest_path[1:no_per_trucks]:
+                orders[i] = ()
+            orders= [t for t in orders if t != ()]
 
-    # ======== START =============
-    i = 0
-    truck_no = 0
-    remove_copy_order_index = []
-    while(number_of_trucks>0 and i<len(all_travel_cost_index)):
-        combi_cities = [orders[j][2] for j in permuts_orders_index[all_travel_cost_index[i][0]]]
-        filter_for = ''.join(combi_cities)
-        if filter_for in ''.join(all_cities):
-
-            # print("MATCH")
-            # print(combi_cities)
-            # print(all_cities)
-            #base case add the of coutries rest in 
-            if(truck_no==len(max_list)-1):
-            #might recalculate to get less travel cost
-                # print("Cities left: ")
-                # print(all_cities)
-                for j in remove_copy_order_index:
-                    orders_left[j] = ()
-                # print("order left: ")
-                orders_left = [t for t in orders_left if t != ()]
-                # print(orders_left)
-
-                all_orders_left_index = ([i for i in range(0,len(orders_left))])
-                permuts_orders_left_index = list(itertools.permutations(all_orders_left_index,len(orders_left)))
-                #calculate all possible travel cost:
-                all_travel_cost_left = []
-                for possible_order in permuts_orders_left_index:
-                    start_cost = int(dict_locations[start_location][orders_left[possible_order[0]][2]])
-                    end_cost = int(dict_locations[orders_left[possible_order[-1]][2]][start_location])
-                    middle_cost = 0
-                    for j in range(0, len(possible_order)-1):
-                        middle_cost+= int(dict_locations[orders_left[possible_order[j]][2]][orders_left[possible_order[j+1]][2]])
-                    all_travel_cost_left.append(start_cost+middle_cost+end_cost)
-                
-                all_travel_cost_left_index = []
-                for j in range(0, len(all_travel_cost_left)):
-                    all_travel_cost_left_index.append([j,all_travel_cost_left[j]])
-              
-                all_travel_cost_left_index.sort(key=lambda x: x[1])
-
-
-                #get first index (cheapest travel cost)
-                for j in permuts_orders_left_index[all_travel_cost_left_index[0][0]]:
-                    max_list[truck_no].append(orders_left[j])
-                number_of_trucks = 0
-            else:
-                #add to trucks
-
-                for j in permuts_orders_index[all_travel_cost_index[i][0]]:
-                    if len(max_list[truck_no])<k-1:
-                        max_list[truck_no].append(orders[j])
-                    else:
-                        truck_no+=1
-                        max_list[truck_no].append(orders[j])
-                        number_of_trucks -= 1
-                    remove_copy_order_index.append(j)
-            for city in combi_cities:
-                try:
-                    if city in all_cities:
-                        all_cities.remove(city)
-                except:
-                    pass
-        i+=1
-
+            max_list_index += 1
+            number_of_trucks -= 1
 
     return max_list
-
-
--- utilities.py
+++ utilities.py
@@ -1,6 +1,78 @@
 # do NOT submit this file
 import numpy as np
 
+# for scoring Q1. score = furthest distance taken by all trucks (smaller is better)
+# if verbose is True, this function prints out the distance travelled by each truck
+def get_score_q1(locations_file, start, orders, result, verbose = False):
+  locmap = read_map(locations_file)
+
+  #Compute shortest distance between START and any delivery location, between any 2 delivery location
+  fulldest = {}
+  compact = [start]
+
+  for order in orders:
+      compact.append(order[2])
+      
+  for node in compact:
+      distance = dijkstra(node, locmap)
+      fulldest[node] = {}
+      for tem in compact:
+          fulldest[node][tem] = distance[tem][1]
+  max_travel_time = 0
+
+  for i in range(len(result)):
+      path = result[i]
+      travel_time = 0
+      src = start
+      for k in range(len(path)):
+          dst = path[k][2]
+          travel_time += fulldest[src][dst]
+          src = dst
+      travel_time += fulldest[src][start]
+      
+      if verbose:
+        print("travel time for truck " + str(i+1) + ": " + str(travel_time))
+      
+      if travel_time > max_travel_time:
+          max_travel_time = travel_time
+  return str(max_travel_time)
+
+  
+# for scoring Q2. score = total distance taken by all trucks (smaller is better)
+# if verbose is True, this function prints out the distance travelled by each truck
+def get_score_q2(locations_file, start, orders, result, verbose = False):
+  locmap = read_map(locations_file)
+
+  #Compute shortest distance between START and any delivery location, between any 2 delivery location
+  fulldest = {}
+  compact = [start]
+
+  for order in orders:
+      compact.append(order[2])
+  for node in compact:
+      distance = dijkstra(node, locmap)
+      fulldest[node] = {}
+      for tem in compact:
+          fulldest[node][tem] = distance[tem][1]
+  total_travel_distance = 0
+
+  for i in range(len(result)):
+      path = result[i]
+      travel_distance = 0
+      src = start
+      for k in range(len(path)):
+          dst = path[k][2]
+          travel_distance += fulldest[src][dst]
+          src = dst
+      travel_distance += fulldest[src][start]
+      
+      if verbose:
+        print("distance for truck " + str(i+1) + " : " + str(travel_distance))
+      
+      total_travel_distance += travel_distance
+  
+  return str(total_travel_distance)
+
 # Returns a list read from filename (e.g. location_1.csv) 
 # every element in the list is a line from the CSV file. The first line (header line) is ignored. 
 # for example, if "location_1.csv" is passed in as filename, this function returns: ["USA,CAN,731", "USA,BHM,1623", "USA,CUB,1813"...]
@@ -13,6 +85,79 @@ def read_location(filename):
             my_list.append(line.strip())
     
     return my_list
+   
+# check the validity of results returned by schedule1() in q1.py
+# returns "" if OK. returns a string (error message) if not OK.
+def check_validity_q1(orders, results, num_truck):
+    if len(results) != num_truck:
+        return "Error: the number of schedules in results does not match the number of trucks"
+        
+    tem = []
+    
+    for order in orders:
+        tem.append(order[2]) #delivery location 
+        
+    for path in results:
+        for node in path:
+            if not node[2] in tem:
+                return "Error: There is a delivery location (" + str(node) + ") in results that is not in orders"
+            tem.remove(node[2]) # delivery location 
+            
+    if len(tem) > 0: # not all destinations in orders have been found in results
+        return "Error: Not all orders were fulfilled. These delivery locations have not been visited: " + str(tem)
+        
+    return "" # all OK 
+
+# check the validity of results returned by schedule2() in q2.py
+# returns "" if OK. returns a string (error message) if not OK. 
+def check_validity_q2(capacities, orders, results):
+    if len(capacities) != len(results):
+        return "Error: the number of schedules in results does not match the number of trucks in capacities"
+
+    tem = []
+    
+    for order in orders:
+        tem.append(order[2]) # delivery location 
+        
+    for i in range(len(results)):
+        path = results[i] # path is a schedule 
+        weight = 0
+        for node in path:
+            weight += node[1] # weight of that order 
+            if not node[2] in tem:
+                return "Error: There is a delivery location (" + str(node) + ") in results that is not in orders"
+            tem.remove(node[2]) # delivery location 
+        if weight > capacities[i]:
+            err_msg = "Error: Truck " + str(i + 1) + " is overloaded! Its capacity is only " + str(capacities[i]) + ", but it is carrying orders weighing " + str(weight)
+            return err_msg # error 
+            
+    if len(tem) > 0:
+        return "Error: Not all orders were fulfilled. These delivery locations have not been visited: " + str(tem)
+        
+    return "" # all OK 
+
+# dijkstra's algorithm     
+def dijkstra(start, graph):
+    distance = {}
+    visited = {}
+    #Initialize
+    for loc in graph:
+        distance[loc] = [loc, np.iinfo(np.int64).max]
+        visited[loc] = 0
+    queue = [start]
+    distance[start][0] = start
+    distance[start][1] = 0
+    while len(queue) > 0:
+        node = queue.pop(0)
+        for neighbour, dist in graph[node].items(): # changed by mok
+            if dist >= 0:
+                if distance[neighbour][1] > dist + distance[node][1]:
+                    distance[neighbour][0] = node
+                    distance[neighbour][1] = dist + distance[node][1]
+                if visited[neighbour] == 0:
+                    queue.append(neighbour)
+        visited[node] = 1
+    return distance
     
 # Returns a dictionary read from filename (e.g. location_1.csv) 
 # Key: origin location. value: dictionary of destination/distance. 
@@ -45,7 +190,7 @@ def read_order(filename): #Order No.,Weight,To
 
 # Returns a list of load capacities of trucks read from filename (e.g. load_1.csv).
 # Format of order CSV file: truck ID, load (weight). Assumption is that ID will always be 1, 2, 3...
-# example of returned list (read from load_1.csv):
+# example of returned list (read from load_1.csv): [100, 1000, 2000]
 def read_load(filename): #Truck ID,Load
     loads = []
     with open(filename) as test:
@@ -54,76 +199,3 @@ def read_load(filename): #Truck ID,Load
             attrs = line.rstrip().split(',')
             loads.append(int(attrs[1]))
     return loads
-
-# check the validity of results returned by schedule1() in q1.py
-# returns "" if OK. returns a string (error message) if not OK.
-def check_validity_q1(orders, results, num_truck):
-    if len(results) != num_truck:
-        return "Error: the number of schedules in results does not match the number of trucks"
-        
-    tem = []
-    
-    for order in orders:
-        tem.append(order[2]) #delivery location 
-        
-    for path in results:
-        for node in path:
-            if not node[2] in tem:
-                return "Error: There is a delivery location (" + str(node) + ") in results that is not in orders"
-            tem.remove(node[2]) # delivery location 
-            
-    if len(tem) > 0: # not all destinations in orders have been found in results
-        return "Error: Not all orders were fulfilled. These delivery locations have not been visited: " + str(tem)
-        
-    return "" # all OK 
-
-# check the validity of results returned by schedule2() in q2.py
-# returns "" if OK. returns a string (error message) if not OK. 
-def check_validity_q2(loads, orders, results):
-    if len(loads) != len(results):
-        return "Error: the number of schedules in results does not match the number of trucks in loads"
-
-    tem = []
-    
-    for order in orders:
-        tem.append(order[2]) # delivery location 
-        
-    for i in range(len(results)):
-        path = results[i] # path is a schedule 
-        weight = 0
-        for node in path:
-            weight += node[1] # weight of that order 
-            if not node[2] in tem:
-                return "Error: There is a delivery location (" + str(node) + ") in results that is not in orders"
-            tem.remove(node[2]) # delivery location 
-        if weight > loads[i]:
-            err_msg = "Error: Truck " + str(i + 1) + " is overloaded! Its capacity is only " + str(loads[i]) + ", but it is carrying orders weighing " + str(weight)
-            return err_msg # error 
-            
-    if len(tem) > 0:
-        return "Error: Not all orders were fulfilled. These delivery locations have not been visited: " + str(tem)
-        
-    return "" # all OK 
-
-# dijkstra's algorithm     
-def dijkstra(start, graph):
-    distance = {}
-    visited = {}
-    #Initialize
-    for loc in graph:
-        distance[loc] = [loc, np.iinfo(np.int64).max]
-        visited[loc] = 0
-    queue = [start]
-    distance[start][0] = start
-    distance[start][1] = 0
-    while len(queue) > 0:
-        node = queue.pop(0)
-        for neighbour, dist in graph[node].items(): # changed by mok
-            if dist >= 0:
-                if distance[neighbour][1] > dist + distance[node][1]:
-                    distance[neighbour][0] = node
-                    distance[neighbour][1] = dist + distance[node][1]
-                if visited[neighbour] == 0:
-                    queue.append(neighbour)
-        visited[node] = 1
-    return distance
