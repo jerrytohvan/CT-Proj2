8422a1df15b8fcdaa302f455289a805b8854a094
Gabriel Ong <gabriel.ong.2016@sis.smu.edu.sg>
2019-03-25 21:23:45 +0800

    added graph for q1

--- q1.py
+++ q1.py
@@ -1,4 +1,6 @@
 # Team ID: <TODO: fill up>
+from collections import deque, namedtuple
+
 
 def schedule1(locations, start_location, number_of_trucks, orders):
     # TODO: replace the code in this function with your algorithm
@@ -10,15 +12,17 @@ def schedule1(locations, start_location, number_of_trucks, orders):
 
     max_list = []
 
+
     for i in range(number_of_trucks):
         max_list.append([])
 
 
-    loc_dic = store_in_dict(locations)
-
-    print(loc_dic)
+    loc_list = store_in_list(locations)
+    graph = Graph(loc_list)
+    print(graph.dijkstra(start_location, "RUS"))
 
     for i in range(len(orders)):
+
         max_list[i % number_of_trucks].append(orders[i])
 
     return max_list
@@ -36,9 +40,107 @@ def store_in_dict(locations):
             val = lst[2]
             dd[start][dest] = val
     return dd
+#
 
 
-def process_orders(dd,orders):
-    pass
+
+def store_in_list(locations):
+    gra = []
+    for loc in locations:
+        lst = loc.split(",")
+        lst[2] = int(lst[2])
+        gra.append(lst)
+    return gra
+
+
+
+# we'll use infinity as a default distance to nodes.
+inf = float('inf')
+Edge = namedtuple('Edge', 'start, end, cost')
+
+
+def make_edge(start, end, cost=1):
+  return Edge(start, end, cost)
+
+
+class Graph:
+    def __init__(self, edges):
+        # let's check that the data is right
+        wrong_edges = [i for i in edges if len(i) not in [2, 3]]
+        if wrong_edges:
+            raise ValueError('Wrong edges data: {}'.format(wrong_edges))
+
+        self.edges = [make_edge(*edge) for edge in edges]
+
+    @property
+    def vertices(self):
+        return set(
+            sum(
+                ([edge.start, edge.end] for edge in self.edges), []
+            )
+        )
+
+    def get_node_pairs(self, n1, n2, both_ends=True):
+        if both_ends:
+            node_pairs = [[n1, n2], [n2, n1]]
+        else:
+            node_pairs = [[n1, n2]]
+        return node_pairs
+
+    def remove_edge(self, n1, n2, both_ends=True):
+        node_pairs = self.get_node_pairs(n1, n2, both_ends)
+        edges = self.edges[:]
+        for edge in edges:
+            if [edge.start, edge.end] in node_pairs:
+                self.edges.remove(edge)
+
+    def add_edge(self, n1, n2, cost=1, both_ends=True):
+        node_pairs = self.get_node_pairs(n1, n2, both_ends)
+        for edge in self.edges:
+            if [edge.start, edge.end] in node_pairs:
+                return ValueError('Edge {} {} already exists'.format(n1, n2))
+
+        self.edges.append(Edge(start=n1, end=n2, cost=cost))
+        if both_ends:
+            self.edges.append(Edge(start=n2, end=n1, cost=cost))
+
+    @property
+    def neighbours(self):
+        neighbours = {vertex: set() for vertex in self.vertices}
+        for edge in self.edges:
+            neighbours[edge.start].add((edge.end, edge.cost))
+
+        return neighbours
+
+    def dijkstra(self, source, dest):
+        assert source in self.vertices, 'Such source node doesn\'t exist'
+        distances = {vertex: inf for vertex in self.vertices}
+        previous_vertices = {
+            vertex: None for vertex in self.vertices
+        }
+        distances[source] = 0
+        vertices = self.vertices.copy()
+
+        while vertices:
+            current_vertex = min(
+                vertices, key=lambda vertex: distances[vertex])
+            vertices.remove(current_vertex)
+            if distances[current_vertex] == inf:
+                break
+            for neighbour, cost in self.neighbours[current_vertex]:
+                alternative_route = distances[current_vertex] + cost
+                if alternative_route < distances[neighbour]:
+                    distances[neighbour] = alternative_route
+                    previous_vertices[neighbour] = current_vertex
+
+        path, current_vertex = deque(), dest
+        while previous_vertices[current_vertex] is not None:
+            path.appendleft(current_vertex)
+            current_vertex = previous_vertices[current_vertex]
+        if path:
+            path.appendleft(current_vertex)
+        return path
+
+
 
 
